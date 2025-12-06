6da30fe85bb7d3436c651b19438aff63b350a443
aditya prasad <adityaprasad85470@gmail.com>
2025-12-06 09:29:46 +0530

    foodlist react

---
+++ food-list/src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+import image from "./images/mohanlal.jpg"
+function App() {
+  let foodList = ["Good Morning", "Good Afternoon", "Good Evening", "Good Night", "Good Day"];
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
