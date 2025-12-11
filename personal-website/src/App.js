ec6e46b4cc0f814f7a08a7d42035258837a5e2bb
aditya prasad <adityaprasad85470@gmail.com>
2025-12-11 15:49:48 +0530

    feat: initialize personal website with React setup
    - Added logo512.png for application branding.
    - Created manifest.json for PWA configuration.
    - Added robots.txt for web crawling instructions.
    - Implemented App.css for styling the main application.
    - Developed App.js as the main component with a Navbar.
    - Created App.test.js for initial testing of the App component.
    - Added empty about.js, contact.js, and home.js components for future development.
    - Established index.css for global styles.
    - Set up index.js as the entry point for the React application.
    - Included logo.svg for application branding.
    - Implemented reportWebVitals.js for performance tracking.
    - Configured setupTests.js for testing environment setup.

---
+++ personal-website/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <p>Home component </p>
+    </div>
+  );
+}
+
+export default App;
